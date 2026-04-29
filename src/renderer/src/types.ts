f1ed8ab4166fb7951c7253fff8ce1667768689f0
Raphael Golubev <raphael.golubev@gmail.com>
2026-04-29 16:59:15 +0300

    fix: пересобрал типы

--- src/renderer/src/types.ts
+++ src/renderer/src/types.ts
@@ -4,33 +4,62 @@ export type ShapeType = 'sticky' | 'rect' | 'circle'
 
 export type ResizeHandle = 'tl' | 'tr' | 'bl' | 'br' | null
 
-export interface SceneObject {
+interface BaseObject {
   id: string
+  color: string
+  isSelected?: boolean
+}
+
+export interface SceneObject extends BaseObject {
   type: ShapeType
   x: number
   y: number
-  preciseX: number
-  preciseY: number
   width: number
   height: number
-  preciseWidth: number
-  preciseHeight: number
-  color: string
+  preciseX?: number
+  preciseY?: number
+  preciseWidth?: number
+  preciseHeight?: number
   text?: string
-  isSelected: boolean
 }
+
+// export interface SceneObject {
+//   id: string
+//   type: ShapeType
+//   x: number
+//   y: number
+//   preciseX: number
+//   preciseY: number
+//   width: number
+//   height: number
+//   preciseWidth: number
+//   preciseHeight: number
+//   color: string
+//   text?: string
+//   isSelected: boolean
+// }
 
 export type ArrowHead = 'none' | 'arrow' | 'triangle' | 'dot'
 
-export interface ArrowObject {
-  id: string
+export interface ArrowObject extends BaseObject {
   type: 'arrow'
   start: { x: number; y: number }
   end: { x: number; y: number }
-  startHead: ArrowHead
-  endHead: ArrowHead
-  color: string
-  // Опционально: id объектов, которые соединяет стрелка
-  startAttachedId?: string
-  endAttachedId?: string
+  startHead: 'none' | 'arrow' | 'triangle'
+  endHead: 'none' | 'arrow' | 'triangle'
 }
+
+export type CanvasObject = SceneObject | ArrowObject
+
+// export interface ArrowObject {
+//   id: string
+//   type: 'arrow'
+//   start: { x: number; y: number }
+//   end: { x: number; y: number }
+//   startHead: ArrowHead
+//   endHead: ArrowHead
+//   color: string
+//   // Опционально: id объектов, которые соединяет стрелка
+//   startAttachedId?: string
+//   endAttachedId?: string
+// }
